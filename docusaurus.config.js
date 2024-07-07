8add6f6386901a06ff80fbc27e45ff4cc0c8f4c1
Genesis Sorto <genesis.writing4fun@gmail.com>
2024-07-06 21:59:04 -0400

    Add Google tag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,6 +50,10 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-JNZLH832T6',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
